aa06b52cd1eff55ebdf571b2d6f203cbed7c47fd
Miha Vidakovič <miha@vidakovic.si>
2017-11-20 18:55:29 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,3 @@
+window.sr = ScrollReveal();
+sr.reveal('.content', { duration: 900, delay: 140, easing: 'ease-in-out', viewOffset: { bottom: 200 }, origin: 'bottom', scale: 1, distance: '80px' });
+sr.reveal('.image', { duration: 900, delay: 30, easing: 'ease-in-out', viewOffset: { bottom: 200 }, origin: 'bottom', scale: 1, distance: '80px' });
